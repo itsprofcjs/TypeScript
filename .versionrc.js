9cb56d0ff34250560baef891b44c448848890a83
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-03 20:26:45 +0530

    feat(map): Maps to pin point the company and users location

---
+++ .versionrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    header: 'Changes',
+    types: [
+        { type: 'feat', section: 'Features' },
+        { type: 'fix', section: 'Bug Fixes' },
+        { type: 'chore', hidden: true },
+        { type: 'docs', section: 'Documents' },
+        { type: 'style', section: 'Coding Standard' },
+        { type: 'refactor', section: 'Code Refactor' },
+        { type: 'perf', section: 'Performance' },
+        { type: 'test', section: 'Testing' },
+    ],
+};
