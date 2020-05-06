37f133354aab6b262c1781c958bc9bfede3fcf87
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-06 16:56:39 +0530

    feat(sort): Linked list sorting now available!

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,11 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numberCollection = new NumberCollection([10, 3, -5, 0]);
+// const numberCollection = new NumberCollection([10, 3, -5, 0]);
 
-const charactersCollection = new CharactersCollection('XadcgTYSsda');
+// const charactersCollection = new CharactersCollection('XadcgTYSsda');
 
 // const sorter = new Sorter(numberCollection);
 
@@ -12,8 +13,20 @@ const charactersCollection = new CharactersCollection('XadcgTYSsda');
 
 // console.log(numberCollection.data);
 
-const sorter = new Sorter(charactersCollection);
+// const sorter = new Sorter(charactersCollection);
+
+// sorter.sort();
+
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 
 sorter.sort();
 
-console.log(charactersCollection.data);
+linkedList.print();
