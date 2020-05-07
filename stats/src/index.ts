367116145cfb8fda02514b53136bdfe1baca1ccc
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-07 17:19:11 +0530

    fix(stats): Deafult report issue

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,13 +5,14 @@ import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { HtmlReport } from './reportTargets/HtmlReport';
 
-const csvFileReader = new CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 
 matchReader.load();
 
 // const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndPrintReport(matchReader.matches);
