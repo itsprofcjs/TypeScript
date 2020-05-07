df6ebe3b1d9714a08f0bc8bb1b8040394a6a54dd
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-07 17:23:49 +0530

    feat(stats): Default set to csv file reader and html reporting
    Note:
    Default file reader(csv): football.csv
    Default reporting format(html): Html

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,18 +1,9 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
-
-// const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 matchReader.load();
-
-// const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
-// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
-const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndPrintReport(matchReader.matches);
