c07dee2523388f4fdfc4b59fe32ad1a9f095fd2a
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-07 15:55:04 +0530

    feat(stats): Composition based implementation available

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,12 +1,21 @@
-import { CsvFileReader } from './CsvFileReader';
-
 import { dateStringToDate } from './utils';
+
 import { MatchResult } from './MatchResult';
+interface DataReader {
+    read(): void;
+    data: string[][];
+}
 
-export type MatchData = [Date, string, string, number, number, MatchResult, string];
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class MatchReader extends CsvFileReader<MatchData> {
-    mapRow(row: string[]): MatchData {
-        return [dateStringToDate(row[0]), row[1], row[2], +row[3], +row[4], row[5] as MatchResult, row[6]];
+export class MatchReader {
+    matches: MatchData[] = [];
+
+    constructor(public reader: DataReader) {}
+
+    load(): void {
+        this.reader.read();
+
+        this.matches = this.reader.data.map((row: string[]): MatchData => [dateStringToDate(row[0]), row[1], row[2], +row[3], +row[4], row[5] as MatchResult, row[6]]);
     }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,17 @@
 import { MatchResult } from './MatchResult';
+
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
+const csvFileReader = new CsvFileReader('football.csv');
 
-reader.read();
+const matchReader = new MatchReader(csvFileReader);
+
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
