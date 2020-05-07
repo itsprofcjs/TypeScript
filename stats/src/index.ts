880505a47d21660956865d7a48aa605f55be7106
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-07 12:50:09 +0530

    feat(stats): Csv file reader can be used for Match Result

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,18 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-    .readFileSync('football.csv', { encoding: 'utf-8' })
-    .split('\n')
-    .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
+
+reader.read();
+
+console.log(reader.data[0]);
 
 let manUnitedWins = 0;
 
-for (const match of matches) {
-    if (match[1] === 'Man United' && match[5] === 'H') {
+for (const match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === 'A') {
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
