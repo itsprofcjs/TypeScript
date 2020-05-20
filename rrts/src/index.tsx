11e528ad524d1924f65bcc5719db53f76566e9b8
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-20 14:53:25 +0530

    feat(rrts): App with minimal featues

---
+++ rrts/src/index.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+interface AppProps {
+    color?: string;
+}
+
+interface AppState {
+    counter: number;
+}
+
+class App extends Component<AppProps, AppState> {
+    state: AppState = {
+        counter: 0,
+    };
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1 });
+    };
+    render() {
+        return (
+            <div>
+                <button onClick={this.onIncrement}> Increment </button>
+                <button onClick={this.onDecrement}> Decrement </button>
+                {this.state.counter}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
